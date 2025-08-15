b21b81b0ccacd70ee4c3183778a05e44da6a1262
Letícia Diniz <leticiacalixtodiniz@gmail.com>
2025-08-15 16:53:24 -0300

    Adicionando os emprestimos e equipamnetos

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,29 @@
 import express from "express";
+import { SERVER_ROUTES } from "./appconfig.js";
+import UsuarioController from "./Controller/UsuarioController.js";
+import EquipamentoController from "./Controller/EquipamentoController.js";
+import EmprestimoController from "./Controller/EmprestimoController.js";
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.json({ mensagem: "Rota padrão" })
 });
+
+router.get(SERVER_ROUTES.LISTAR_USUARIO, UsuarioController.todos);
+router.get(SERVER_ROUTES.UNICO_USUARIO, UsuarioController.unico);
+router.post(SERVER_ROUTES.NOVO_USUARIO, UsuarioController.cadastrar);
+router.delete(SERVER_ROUTES.REMOVER_USUARIO, UsuarioController.remover);
+router.put(SERVER_ROUTES.ATUALIZAR_USUARIO, UsuarioController.atualizar);
+
+router.get(SERVER_ROUTES.LISTAR_EQUIPAMENTO, EquipamentoController.todos);
+router.post(SERVER_ROUTES.NOVO_EQUIPAMENTO, EquipamentoController.cadastrar);
+router.delete(SERVER_ROUTES.REMOVER_EQUIPAMENTO, EquipamentoController.remover);
+router.put(SERVER_ROUTES.ATUALIZAR_EQUIPAMENTO, EquipamentoController.atualizar);
+
+router.get(SERVER_ROUTES.LISTAR_EMPRESTIMOS, EmprestimoController.todos);
+router.get(SERVER_ROUTES.LISTAR_EMPRESTIMOS_DETALHADO, EmprestimoController.listarDetalhados);
+router.post(SERVER_ROUTES.NOVO_EMPRESTIMO, EmprestimoController.cadastrar);
+router.put(SERVER_ROUTES.ATUALIZAR_EMPRESTIMO, EmprestimoController.atualizar);
 
 export { router }
